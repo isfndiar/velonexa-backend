7a0450851d828ac11f97fa272484218ccbf5c829
rafi tanujaya <rafibussines@gmail.com>
2024-08-01 21:52:21 +0700

    Adding migration create table follows

---
+++ migrations/1722521299363_create-follow-table.js
@@ -0,0 +1,60 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('follows', {
+    follower_id: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+      references: 'users',
+      onDelete: 'CASCADE',
+    },
+    following_id: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+      references: 'users',
+      onDelete: 'CASCADE',
+    },
+    created_at: {
+      type: 'TIMESTAMPTZ',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+
+  pgm.addConstraint('follows', 'fk_follower_user', {
+    foreignKeys: {
+      columns: 'follower_id',
+      references: 'users(id)',
+      onDelete: 'CASCADE',
+    },
+  });
+
+  pgm.addConstraint('follows', 'fk_following_user', {
+    foreignKeys: {
+      columns: 'following_id',
+      references: 'users(id)',
+      onDelete: 'CASCADE',
+    },
+  });
+
+  pgm.addConstraint('follows', 'follows_pkey', {
+    primaryKey: ['follower_id', 'following_id'],
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('follows');
+};
