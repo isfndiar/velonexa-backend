68e477dbefae714e8a0e27754e3a373c92f1fe56
rafi tanujaya <rafibussines@gmail.com>
2024-08-13 19:53:37 +0700

    Adding file declaration for env

---
+++ env.d.ts
@@ -0,0 +1,19 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test';
+    DATABSE_URL: string;
+    DB_HOST: string;
+    DB_PORT: string;
+    DB_USER: string;
+    DB_PASSWORD: string;
+    DB_NAME: string;
+    DATABASE: string;
+    PORT: string;
+    BCRYPT_SALT: string;
+    JWT_SECRET: string;
+    SUPABASE_URL: string;
+    SUPABASE_KEY: string;
+    IMAGE_PROFILE_DEFAULT_URL: string;
+    BASE_IMAGE_PROFILE_URL: string;
+  }
+}
